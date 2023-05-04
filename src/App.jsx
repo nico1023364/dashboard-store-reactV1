be15a515305073365e471e6d2759a9f0d76b987e
Nicolas <nialfonso12nick@gmail.com>
2023-05-04 17:14:01 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { FaAccessibleIcon } from "react-icons/fa";
+function App() {
+  return <div className=" App bg-red-400">
+    Hola Mundo <FaAccessibleIcon size="4rem" />
+  </div>;
+}
+
+export default App;
