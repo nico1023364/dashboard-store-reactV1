9b57db9ec6acceac9c86b1c606a20053823dd5c2
Nicolas <nialfonso12nick@gmail.com>
2023-05-04 22:23:14 -0500

    sidevar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-import { FaAccessibleIcon } from "react-icons/fa";
+import {useState} from 'react'
+import Sidebar from "./components/shared/Sidebar";
+import { RiMenu3Fill, RiUser3Line, RiAddLine, RiPieChartLine } from "react-icons/ri";
 function App() {
-  return <div className=" App bg-red-400">
-    Hola Mundo <FaAccessibleIcon size="4rem" />
-  </div>;
+
+  const [showMenu, setShowMenu] = useState(false)
+  const [showOrder, setShowOrder] = useState(false)
+  
+  const toogleMenu = () =>{
+    setShowMenu(!showMenu)
+  }
+  return (
+    <div className="bg-[#262837] w-full h-[100vh]">
+      <Sidebar />
+      {/* Menu movil */}
+      <nav className="bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-400 py-4 px-8 flex items-center justify-between rounded-tl-xl rounded-tr-xl">
+        <button>
+          <RiUser3Line />
+        </button>
+        <button>
+          <RiAddLine />
+        </button>
+        <button>
+          <RiPieChartLine />
+        </button>
+        <button className="text-white">
+          <RiMenu3Fill />
+        </button>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/shared/Sidebar.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import {
+  RiHome6Line,
+  RiPercentLine,
+  RiPieChartLine,
+  RiMailLine,
+  RiNotification3Line,
+  RiSettings4Line,
+  RiLogoutCircleRLine,
+  RiRestaurantLine,
+} from "react-icons/ri";
+
+const Sidebar = (props) => {
+  const { showMenu } = props;
+
+  return (
+    <div className="bg-[#1F1D2B] fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between py-5 rounded-tr-2xl rounded-br-2xl z-50 -left-full">
+      <div>
+        <ul className="px-4">
+          <li className="logo">
+            <span className="text-gray-300 text-3xl font-bold flex justify-center my-5 p-4  ">
+              <RiRestaurantLine />
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div className="sidebar-up">
+        <ul className="pl-4">
+          <li className="bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl">
+            <a
+              href="#"
+              title="home"
+              className="bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-white"
+            >
+              <RiHome6Line className="text-2xl" />
+            </a>
+          </li>
+          <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
+            >
+              <RiPercentLine className="text-2xl" />
+            </a>
+          </li>
+          <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
+            >
+              <RiPieChartLine className="text-2xl" />
+            </a>
+          </li>
+          <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
+            >
+              <RiMailLine className="text-2xl" />
+            </a>
+          </li>
+          <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
+            >
+              <RiNotification3Line className="text-2xl" />
+            </a>
+          </li>
+          <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              data-tooltip="Este es un ejemplo de tooltip"
+              className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
+            >
+              <RiSettings4Line className="text-2xl" />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul className="pl-4">
+          <li className="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors"
+            >
+              <RiLogoutCircleRLine className="text-2xl" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
